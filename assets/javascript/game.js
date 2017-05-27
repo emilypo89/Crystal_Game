f97a3977b9b299cb65f9376407db1804de756a71
emilypo89 <emilypo89@gmail.com>
2017-05-27 13:48:31 -0400

    generate random number

---
+++ assets/javascript/game.js
@@ -0,0 +1,86 @@
+// have the computer generate a random number
+// have that number display in the "computerGuess" div
+
+$(document).ready(function(){
+
+var userGuessArray = [];
+var boolean = false;
+var computerGuess = Math.floor((Math.random() * 120) + 19);
+	$("#computerGuess").html(computerGuess);
+	console.log(computerGuess);
+
+// generate random number on button click of crystal images
+
+     
+      $("crystal1").on("click", function() {
+      
+        var randomValue = (Math.floor(Math.random()*13)+1);
+       $("#userGuess").html(randomValue);
+       console.log(randomValue);
+
+       		// var crystal1guess = parsInt(randomValue);
+       		// if (randomValue === guess1){
+
+       		// }
+
+      });
+
+       $("crystal1").on("click", function() {
+      
+        var randomValue = (Math.floor(Math.random()*13)+1);
+       $("#userGuess").html(randomValue);
+       console.log(randomValue);
+
+       		// var crystal1guess = parsInt(randomValue);
+       		// if (randomValue === guess1){
+
+       		// }
+
+      });
+
+       $("crystal1").on("click", function() {
+      
+        var randomValue = (Math.floor(Math.random()*13)+1);
+       $("#userGuess").html(randomValue);
+       console.log(randomValue);
+
+       		// var crystal1guess = parsInt(randomValue);
+       		// if (randomValue === guess1){
+
+       		// }
+
+      });
+
+        $("crystal1").on("click", function() {
+      
+        var randomValue = (Math.floor(Math.random()*13)+1);
+       $("#userGuess").html(randomValue);
+       console.log(randomValue);
+
+       		// var crystal1guess = parsInt(randomValue);
+       		// if (randomValue === guess1){
+
+       		// }
+
+      });
+
+// add the random numbers together
+      function addValue(randomValue) {
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });
+
